45abfd0acd4db35ccdefabadf0d89877a504bbe9
Shadowx9920 <youssefoutahar1@gmail.com>
2022-07-04 16:00:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tutorat2022
+ WebApp Project for UIR
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const WebApp());
+}
+
+class WebApp extends StatelessWidget {
+  const WebApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
